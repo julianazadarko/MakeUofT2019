6acd3e505a0f4a4eac34a7dca647612b0a9af749
julianazadarko <43300146+julianazadarko@users.noreply.github.com>
2019-02-16 16:21:33 -0500

    IP

---
+++ getArduinoIp/getArduinoIp.ino
@@ -0,0 +1,29 @@
+#include <WiFi101.h>
+
+char ssid[] = "yourNetwork";      //SSID of your network
+
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+IPAddress ip;                    // the IP address of your shield
+
+void setup()
+{
+ // initialize serial:
+ Serial.begin(9600);
+
+ WiFi.begin(ssid);
+
+  if ( status != WL_CONNECTED) { 
+    Serial.println("Couldn't get a wifi connection");
+    while(true);
+  } 
+  // if you are connected, print out info about the connection:
+  else {
+ //print the local IP address
+  ip = WiFi.localIP();
+  Serial.println(ip);
+
+  }
+}
+
+void loop () {}
